25801148e7323dd1dee2bcc62787b539ce4549cc
Manuel Sousa <manuelsousamvs@gmail.com>
2019-12-20 01:15:23 +0000

    Add data processing

--- App/example/lib/charts.dart
+++ App/example/lib/charts.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:bezier_chart/bezier_chart.dart';
 import 'package:intl/intl.dart' as intl;
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:math';
 
 class ChartScreen extends StatelessWidget {
   @override
@@ -296,7 +295,7 @@ Widget hourly(BuildContext context) {
           displayYAxis: true,
           stepsYAxis: 25,
           footerHeight: 35.0,
-          backgroundColor: Colors.lightGreen,
+          backgroundColor: Colors.lightBlue,
         ),
       ),
     ),
@@ -307,22 +306,49 @@ Widget daily(BuildContext context) {
   final fromDate = DateTime(2019, 09, 01);
   final toDate = DateTime(2019, 09, 31);
 
-  final current = DateTime(2019, 09, 10);
-
-  final date1 = DateTime.now().subtract(Duration(days: 2));
-  final date2 = DateTime.now().subtract(Duration(days: 3));
-
-  final date3 = DateTime.now().subtract(Duration(days: 35));
-  final date4 = DateTime.now().subtract(Duration(days: 36));
-
-  final date5 = DateTime.now().subtract(Duration(days: 65));
-  final date6 = DateTime.now().subtract(Duration(days: 64));
-
-
   final databaseReference = Firestore.instance;
 
+  DateTime getDataFromDate() {
+    var low = DateTime(2020, 09, 01);
+    databaseReference
+        .collection("metrics")
+        .getDocuments()
+        .then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((DocumentSnapshot doc) {
+        for(var doc in snapshot.documents){
+          for (var f in doc.data.keys){
+            if(DateTime.parse(f).millisecondsSinceEpoch < low.millisecondsSinceEpoch){
+              low = DateTime.parse(f);
+            }
+
+          }
+        }
+      });
+    });
+    return low;
+  }
+
+  DateTime getDataToDate() {
+    var high = DateTime(1990, 09, 01);
+    databaseReference
+        .collection("metrics")
+        .getDocuments()
+        .then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((DocumentSnapshot doc) {
+        for(var doc in snapshot.documents){
+          for (var f in doc.data.keys){
+            if(DateTime.parse(f).millisecondsSinceEpoch > high.millisecondsSinceEpoch){
+              high = DateTime.parse(f);
+            }
+
+          }
+        }
+      });
+    });
+    return high;
+  }
+
   List<DataPoint<dynamic>> getData() {
-    var rng = new Random();
     List<DataPoint<DateTime>> list = new List<DataPoint<DateTime>>();
     databaseReference
         .collection("metrics")
@@ -330,18 +356,35 @@ Widget daily(BuildContext context) {
         .then((QuerySnapshot snapshot) {
 
       snapshot.documents.forEach((DocumentSnapshot doc) {
-        doc.data.forEach((k,v) => {
-          print('${DateTime.parse(k)}'),
-          list.add(new DataPoint<DateTime>(value: rng.nextInt(100).toDouble(), xAxis: DateTime.parse(k))),
-        });
+        for(var doc in snapshot.documents){
+          for (var f in doc.data.keys){
+            var score = 1.1;
+            score += doc[f]["driving"] > 0 ? 30 : 0;
+            score += doc[f]["bus"] > 0 ? 20 : 0;
+            score += doc[f]["train"] > 0 ? 10 : 0;
 
+
+            score -= doc[f]["walking"] > 0 ? 10 : 0;
+            score -= doc[f]["cycling"] > 0 ? 10 : 0;
+            score -= doc[f]["moving"] > 0 ? 10 : 0;
+
+            if(doc[f]["flying"] > 0){
+              score = 100;
+            }
+            score = score < 0 ? 0 : score;
+            score = score > 100 ? 100 : score;
+            list.add(new DataPoint<DateTime>(value: score.toDouble(), xAxis: DateTime.parse(f)));
+          }
+        }
       });
     });
-    print(date6.toString());
-    print("fodasse");
     print(list);
+//    print(getDataFromDate());
+//    print(getDataToDate());
     return list;
   }
+
+
 
   return Center(
     child: Container(
@@ -363,6 +406,9 @@ Widget daily(BuildContext context) {
           BezierLine(
             label: "",
             data: getData(),
+            onMissingValue: (dateTime) {
+              return 0;
+            },
           ),
         ],
         config: BezierChartConfig(
@@ -373,9 +419,9 @@ Widget daily(BuildContext context) {
           verticalIndicatorColor: Colors.black26,
           showVerticalIndicator: true,
           displayYAxis: true,
-          stepsYAxis: 5,
+          stepsYAxis: 20,
           verticalIndicatorFixedPosition: false,
-          backgroundColor: Colors.lightGreen,
+          backgroundColor: Colors.lightBlue,
         ),
       ),
     ),
@@ -384,16 +430,7 @@ Widget daily(BuildContext context) {
 
 Widget monthly(BuildContext context) {
   final fromDate = DateTime(2019, 01, 01);
-  final toDate = DateTime.now();
-
-  final date1 = DateTime.now().subtract(Duration(days: 2));
-  final date2 = DateTime.now().subtract(Duration(days: 3));
-
-  final date3 = DateTime.now().subtract(Duration(days: 35));
-  final date4 = DateTime.now().subtract(Duration(days: 36));
-
-  final date5 = DateTime.now().subtract(Duration(days: 65));
-  final date6 = DateTime.now().subtract(Duration(days: 64));
+  final toDate = DateTime(2019, 12, 01);
 
   return Center(
     child: Container(
@@ -414,12 +451,18 @@ Widget monthly(BuildContext context) {
               return 5.0;
             },
             data: [
-              DataPoint<DateTime>(value: 10, xAxis: date1),
-              DataPoint<DateTime>(value: 50, xAxis: date2),
-              DataPoint<DateTime>(value: 20, xAxis: date3),
-              DataPoint<DateTime>(value: 80, xAxis: date4),
-              DataPoint<DateTime>(value: 14, xAxis: date5),
-              DataPoint<DateTime>(value: 30, xAxis: date6),
+              DataPoint<DateTime>(value: 70, xAxis: DateTime(2019, 01, 01)),
+              DataPoint<DateTime>(value: 65, xAxis: DateTime(2019, 02, 01)),
+              DataPoint<DateTime>(value: 60, xAxis: DateTime(2019, 03, 01)),
+              DataPoint<DateTime>(value: 70, xAxis: DateTime(2019, 04, 01)),
+              DataPoint<DateTime>(value: 50, xAxis: DateTime(2019, 05, 01)),
+              DataPoint<DateTime>(value: 60, xAxis: DateTime(2019, 06, 01)),
+              DataPoint<DateTime>(value: 40, xAxis: DateTime(2019, 07, 01)),
+              DataPoint<DateTime>(value: 30, xAxis: DateTime(2019, 08, 01)),
+              DataPoint<DateTime>(value: 40, xAxis: DateTime(2019, 09, 01)),
+              DataPoint<DateTime>(value: 70, xAxis: DateTime(2019, 10, 01)),
+              DataPoint<DateTime>(value: 80, xAxis: DateTime(2019, 11, 01)),
+              DataPoint<DateTime>(value: 50, xAxis: DateTime(2019, 12, 01)),
             ],
           ),
         ],
@@ -431,7 +474,7 @@ Widget monthly(BuildContext context) {
           displayYAxis: true,
           stepsYAxis: 25,
           footerHeight: 40.0,
-          backgroundColor: Colors.lightGreen,
+          backgroundColor: Colors.lightBlue,
         ),
       ),
     ),
